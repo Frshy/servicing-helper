551d66ab2f3f63e7f6856bcfc95f8cca90750317
Freshy <79219288+Frshy@users.noreply.github.com>
2023-12-16 00:36:37 +0100

    Delete auth-microservice directory

--- auth-microservice/src/auth/auth.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class AuthResolver {
-    @Query(() => String)
-    witajSwiecie() {
-        return 'Lorem Ipsum ZGlkbnQgZXhwZWN0IHRoYXQgaWYgaSBkb250IHNldCByYW0gYWxsb2NhdGlvbiBsaW1pdCBpbiB3c2wncyBjb25maWcgZmlsZSB3aGljaCBkaWRudCBldmVuIGV4aXN0IG9uZSBkb2NrZXIncyBjb250YWluZXIgd291bGQgdXNlIDEwZ2Igb2YgcmFtIGNhc3VpbmcgbXkgcGMgKGFuZCBteSBtZW50YWwpIGNyYXNoaW5n';
-    }
-}
